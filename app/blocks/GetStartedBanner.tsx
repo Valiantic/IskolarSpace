e3286afe645a8c89f66960eb3ae48cc17a9458b3
StevenMadali <stevenmadali17@gmail.com>
2025-06-16 22:50:09 +0800

    Modify: Get Started Banner Background Pic

--- app/blocks/GetStartedBanner.tsx
+++ app/blocks/GetStartedBanner.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useEffect } from 'react';
 import Link from 'next/link';
+import GetStartedPic from '../../public/images/GetStartedPic.png';
 
 const GetStartedBanner = () => {
 
@@ -10,25 +11,34 @@ const GetStartedBanner = () => {
       AOS.init({
         duration: 2000,
         once: true,
-      });
-    }
+      });    }
     
     initAOS();
-  })
-
+  });
+  
   return (
-    <section data-aos="fade-up" className='bg-gradient-to-r from-white to-sky-500 rounded-md'>
-  <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
-    <div className="mx-auto max-w-xl text-center">
-      <h1 className="text-4xl text-gray-900 font-extrabold sm:text-4xl">
-        Empower your academic journey with 
-        <strong className="font-extrabold sm:block text-zinc-50"> IskolarSpace today! </strong>
-      </h1>
+    <section 
+      data-aos="fade-up" 
+      className="rounded-md relative"
+      style={{ 
+        backgroundImage: `url(${GetStartedPic.src})`, 
+        backgroundSize: "cover",
+        backgroundPosition: "center" 
+      }}
+    >      <div 
+        className="absolute inset-0 bg-black bg-opacity-50"
+      ></div>
+      <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center relative z-10">
+        <div className="mx-auto max-w-xl text-center">
+          <h1 className="text-4xl text-white font-extrabold sm:text-4xl shadow-text">
+            Empower your academic journey with 
+            <strong className="font-extrabold sm:block text-cyan-400"> IskolarSpace today! </strong>
+          </h1>
 
-      <div className="mt-8 flex flex-wrap justify-center gap-4">
-        <Link href="/signup" className="block w-full rounded-md bg-cyan-600 px-12 py-3 text-sm font-medium text-white shadow-sm hover:bg-cyan-700 focus:ring-3 focus:outline-hidden sm:w-auto">
-        Get Started
-        </Link>
+          <div className="mt-8 flex flex-wrap justify-center gap-4">
+            <Link href="/signup" className="block w-full rounded-md bg-cyan-600 px-12 py-3 text-sm font-medium text-white shadow-sm hover:bg-cyan-700 focus:ring-3 focus:outline-hidden sm:w-auto">
+            Get Started
+            </Link>
 
       </div>
     </div>
