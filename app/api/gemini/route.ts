b3b1b4ab28a705b9d0b8c377a332fcf923456643
StevenMadali <stevenmadali17@gmail.com>
2025-10-12 21:40:43 +0800

    Modify: Update Model and License

--- app/api/gemini/route.ts
+++ app/api/gemini/route.ts
@@ -7,7 +7,7 @@ export async function POST(req: Request) {
 
     try {
         const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
-        const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash'});
+        const model = genAI.getGenerativeModel({ model: 'gemini-2.5-flash'});
 
         const result = await model.generateContent(prompt);
         const plan = result.response.text();
